cce793d215e4c166c0c6a8b281dec3651d1086db
lixinwei <lixinwei@vipkid.com.cn>
2021-01-21 02:41:20 +0800

    testdemo

---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+# _*_ coding: utf-8 -*-
+# @Author : lixinwei
+# @Time   : 2021/1/21 2:29 上午
---
+++ test/testBaidu/testBaidu.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 -*-
+# @Author : lixinwei
+# @Time   : 2021/1/21 2:29 上午
+import unittest
+
+class TestBaidu(unittest.TestCase):
+    def test_baidu(self):
+        print("www.baidu.com")
+        assert (1,1)
+    def test_baidu_two(self):
+        pass
